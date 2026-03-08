781c200b641ddd13e81bbac9b1f8b1a214014976
Håkon Hapnes Strand <haakon@headroom.as>
2026-03-08 22:18:18 +0100

    Bump version with new build

--- gui/version.py
+++ gui/version.py
@@ -3,5 +3,5 @@ Auto-generated version file
 Generated at build time with UTC timestamp
 """
 
-BUILD_VERSION = "v2026.03.06.2129"
-BUILD_TIME_UTC = "2026-03-06T21:29:31.399025+00:00"
+BUILD_VERSION = "v2026.03.08.2115"
+BUILD_TIME_UTC = "2026-03-08T21:15:40.208077+00:00"
